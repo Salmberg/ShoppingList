06b0ed7ae25e7b7aedecdf657e241f0308e40261
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-17 15:27:12 +0200

    Now with auth Anonymous

--- ShoppingList/ContentView.swift
+++ ShoppingList/ContentView.swift
@@ -8,7 +8,42 @@
 import SwiftUI
 import Firebase
 
-struct ContentView: View {
+struct ContentView : View {
+    
+    @State var singedIn = false
+    
+    var body : some View {
+        if !singedIn {
+            SignInView(signedIn: $singedIn)
+        }else {
+            ShoppingListView()
+        }
+    }
+}
+
+
+struct SignInView : View {
+    @Binding var signedIn : Bool
+    var auth = Auth.auth()
+    
+    var body : some View {
+        Button(action: {
+            auth.signInAnonymously { result, error in
+                if let error = error {
+                    print("Error signing in")
+                    
+                } else{
+                    signedIn = true
+                }
+            }
+            
+        }) {
+            Text("Sign in")
+        }
+    }
+}
+
+struct ShoppingListView: View {
     
    @StateObject var shoppingListVM = ShoppingListVM()
     
@@ -17,31 +52,35 @@ struct ContentView: View {
           
             List {
                 ForEach(shoppingListVM.items) { item in
-                    HStack {
-                        Text(item.name)
-                        Spacer()
-                        Button(action: {
-                            shoppingListVM.toggle(item: item)
-                            
-                        }) {
-                            Image(systemName: item.done ? "checkmark.circle.fill" : "circle")
-                        }
-                        
-                    }
-                    
+                    RowView(item: item, vm: shoppingListVM)
                 }
             }
-            
             
         }.onAppear(){
             shoppingListVM.listenToFirestore()
         }
     }
-    
- 
-        
+
     }
 
+struct RowView: View {
+    let item : Item
+    let vm : ShoppingListVM
+    
+    var body: some View {
+        HStack {
+            Text(item.name)
+            Spacer()
+            Button(action: {
+                vm.toggle(item: item)
+                
+            }) {
+                Image(systemName: item.done ? "checkmark.circle.fill" : "circle")
+            }
+            
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
