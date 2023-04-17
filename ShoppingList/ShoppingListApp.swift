e4092c1b7ddd86fb5b04fcf59e75a550dc8a5d14
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-17 13:31:16 +0200

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by David Salmberg on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
